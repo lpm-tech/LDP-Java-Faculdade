a465576b010ec91c35f82beda7c7cb7a340e0e0a
Láuren Pimenta <lauren.pm02@gmail.com>
2026-06-15 08:40:34 -0300

    Refactor Exercicio_05 for temperature conversion

---
+++ Exercícios_Estrutura_Sequencial/Exercicio_05.java
@@ -0,0 +1,26 @@
+/* Estrutura Sequencial 
+//Exercício 5 - Faça um programa que receba uma temperatura em Celsius, calcule e mostre essa temperatura em Fahrenheit 
+(F = (C*1,8) + 32)
+*/
+
+import java.util.Scanner;
+public class Exercicio_05
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    
+	    //Variáveis
+	    double c, f;
+	    
+	    //Entrada
+		  System.out.println("Digite a temperatura em Celsius:");
+		  c=sc.nextInt();
+		
+		  //Processamento
+		  f = (c*1.8) + 32;
+		
+		  //Saída
+		  System.out.println("Temperatura inserida em Celsius: "+c);
+		  System.out.println("A temperatura em Fahrenheit é: "+f);
+	}
+}
